2562911a191fb8fc6e79cb33b7de7116fe976972
kevinAstilla <kevinastilla@gmail.com>
2021-08-06 23:24:59 -0400

    added the plant class, models folder, and data folder

---
+++ theleafdiary.website/models/Plant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace theleafdiary.website.models
+{
+    public class Plant
+    {
+        public string name { get; set; }
+        public int zoneId { get; set; }
+        public string description { get; set; }
+        [JsonPropertyName("img")]
+        public string image { get; set; }
+        public int lightTypeId { get; set; }
+        public string soilTypeId { get; set; }
+        public int[] careDifficulty { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Plant>(this);
+    }
+}
